14f762221147d813c64e203a7f8a448a2bb4f936
Taylor Smith <tsmith@tsmithcreative.com>
2022-05-08 10:03:57 -0500

    Fix the find-unused script and add/remove stuff

--- bin/find-unused.sh
+++ bin/find-unused.sh
@@ -6,5 +6,5 @@ export $(grep -v '^#' .env | xargs)
 find photos/ -type f | sed --expression='s/photos\///g' | while read -r photo
 do
   [[ "$photo" == "metadata.json" ]] && continue
-  grep -r "$photo" _adventures/ _hobbies/ > /dev/null || echo $photo is unused
+  grep -r "$photo" _posts/ > /dev/null || echo $photo is unused
 done
