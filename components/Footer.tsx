ad2e2bca2cf532b7bbc31daafb09fde79ffddaec
Taylor Smith <tsmith@tsmithcreative.com>
2022-05-12 00:16:33 -0500

    Social media icons in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,6 +12,17 @@ export const Footer = () => {
         </a>
         .
       </div>
+      <div className={style.socialIcons}>
+        <a href="https://instagram.com/tsmith512">
+          <img src={require('../public/gfx/instagram.svg')} aria-label="Follow me on Instagram" />
+        </a>
+        <a href="https://www.linkedin.com/in/tsmith512">
+          <img src={require('../public/gfx/linkedin.svg')} aria-label="My profile on LinkedIn" />
+        </a>
+        <a href="https://github.com/tsmith512">
+          <img src={require('../public/gfx/github.svg')} aria-label="My work on GitHub" />
+        </a>
+      </div>
     </footer>
   );
 };
